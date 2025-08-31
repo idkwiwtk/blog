5f7c74e9c5cf718d84970bc3e7ef2a9c45998b5a
idkwiwtk <bh5619@naver.com>
2025-08-31 13:31:31 +0900

    noto sans 폰트 cdn 임포트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+
         {/* 나눔 글꼴 */}
         <link
           href="https://hangeul.pstatic.net/hangeul_static/css/nanum-gothic.css"
